b54ee21eade5098fe3a48fb5155c94acb64c0dd5
Veleslavia <okalistratova@gmail.com>
2019-05-21 12:56:38 +0200

    improve readability of the testcases

--- veleslavia/assignment-1/anagramchecker_test.py
+++ veleslavia/assignment-1/anagramchecker_test.py
@@ -6,31 +6,31 @@ from anagramchecker import AnagramChecker
 class TestAnagramChecker(unittest.TestCase):
     def test_identical_strings(self):
         checker = AnagramChecker(case_sensitive=False, each_word_in_sentence=False)
-        self.assertEqual(checker.check("test", "test"), True)
+        self.assertTrue(checker.check("test", "test"))
 
     def test_basic(self):
         checker = AnagramChecker(case_sensitive=False, each_word_in_sentence=False)
-        self.assertEqual(checker.check("listen", "silent"), True)
-        self.assertEqual(checker.check("triangle", "integral"), True)
-        self.assertEqual(checker.check("apple", "pabble"), False)
+        self.assertTrue(checker.check("listen", "silent"))
+        self.assertTrue(checker.check("triangle", "integral"))
+        self.assertFalse(checker.check("apple", "pabble"))
 
     def test_preprocessing(self):
         checker = AnagramChecker(case_sensitive=False, each_word_in_sentence=False)
-        self.assertEqual(checker.check("test", "test,  "), True)
+        self.assertTrue(checker.check("test", "test,  "))
 
     def test_mismatched_len(self):
         checker = AnagramChecker(case_sensitive=False, each_word_in_sentence=False)
-        self.assertEqual(checker.check("test", "testt"), False)
+        self.assertFalse(checker.check("test", "testt"))
 
     def test_case_sensitive(self):
         checker = AnagramChecker(case_sensitive=True, each_word_in_sentence=False)
-        self.assertEqual(checker.check("listen", "Silent"), False)
+        self.assertFalse(checker.check("listen", "Silent"))
 
     def test_sentences(self):
         checker = AnagramChecker(case_sensitive=False, each_word_in_sentence=False)
-        self.assertEqual(checker.check("Astronomer", "Moon starer"), True)
+        self.assertTrue(checker.check("Astronomer", "Moon starer"))
 
     def test_each_word_in_sentence(self):
         checker = AnagramChecker(case_sensitive=False, each_word_in_sentence=True)
-        self.assertEqual(checker.check("Astronomer", "Moon starer"), False)
-        self.assertEqual(checker.check("Bored cat night", "Robed act thing"), True)
+        self.assertFalse(checker.check("Astronomer", "Moon starer"))
+        self.assertTrue(checker.check("Bored cat night", "Robed act thing"))

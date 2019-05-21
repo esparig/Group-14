b8c3a4b55edebbaab0c5d583477f59c33eb1d84b
esparig <esparig@gmail.com>
2019-05-21 14:11:51 +0200

    Code updated to follow PyLint conventions

--- esparig/assignment-1/anagrams.py
+++ esparig/assignment-1/anagrams.py
@@ -1,16 +1,25 @@
-import collections
+"""First assignment: Anagrams"""
+from collections import Counter
 
 def anagrams_case_sensitive(str1: str, str2: str) -> bool:
-    a = collections.Counter(str1)
-    for c in str2:
-        a[c] -= 1
-        if a[c] < 0:
+    """Function to analyze if two strings are anagrams"""
+    dict1 = Counter(str1)
+    for char in str2:
+        dict1[char] -= 1
+        if dict1[char] < 0:
             return False
-    return sum(a.values()) == 0
+    return sum(dict1.values()) == 0
 
-def anagrams(str1: str, str2: str, case_insensitive: bool=False) -> bool:
+
+def anagrams(str1: str, str2: str, case_insensitive: bool = False,
+             is_sentence: bool = False, corresponding_words: bool = False) -> bool:
+    """Fuction to analyze if two strings are anagrams considering case sensitivity and sentences"""
+    if is_sentence:
+        if corresponding_words:
+            pass #TO DO
+        else:
+            str1 = ''.join([i for i in str1 if i.isalnum()])
+            str2 = ''.join([i for i in str2 if i.isalnum()])
     if case_insensitive:
         return anagrams_case_sensitive(str1.lower(), str2.lower())
-    else:
-        return anagrams_case_sensitive(str1, str2)
-    
+    return anagrams_case_sensitive(str1, str2)
--- esparig/assignment-1/anagrams_test.py
+++ esparig/assignment-1/anagrams_test.py
@@ -1,18 +1,24 @@
+"""Unit tests for first assignment"""
 import unittest
 
-from anagrams import *
+from anagrams import anagrams
 
 class TestAnagrams(unittest.TestCase):
+    """Unit Test Class for Anagrams"""
 
     def test_case_sensitive(self):
+        """Tests for case sensitive strings"""
         self.assertTrue(anagrams("silent", "listen"))
         self.assertFalse(anagrams("silent", "listennn"))
         self.assertFalse(anagrams("silentt", "listen"))
         self.assertFalse(anagrams("Silent", "listen", case_insensitive=False))
 
-        
-
     def test_case_insensitive(self):
+        """Tests for case insensitive strings"""
         self.assertTrue(anagrams("Silent", "listen", case_insensitive=True))
         self.assertFalse(anagrams("silent", "listennn", case_insensitive=True))
-    
+
+    def test_case_sentences(self):
+        """Tests for sentences"""
+        self.assertTrue(anagrams("So dark the con of man", "Madonna of the Rocks",
+                                 case_insensitive=True, is_sentence=True))

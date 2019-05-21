f3b65e5b38250e96351e17ad4346828a077193b6
Daniela Ivanova <daniela.ivanova21@gmail.com>
2019-05-21 22:07:30 +0100

    Added some comments.

--- daniela997/assignment-1/anagramchecker.py
+++ daniela997/assignment-1/anagramchecker.py
@@ -1,17 +1,34 @@
+"""This module defines an Anagram checker implementation."""
 import string
 
 
 class AnagramChecker(object):
+    """Anagram checker with basic functionality.
+
+    Assumes that character case does not matter,
+    and inputs should be single words made up of
+    alphabetical characters.
+    """
     def __init__(self):
-            self.table = dict.fromkeys(string.ascii_lowercase, 0)
+        """Creates an anagram checker.
+        """
+        self.table = dict.fromkeys(string.ascii_lowercase, 0)
 
     def check_anagrams(self, sequence1, sequence2):
+        """Checks two sequences are anagrams.
+        """
         if len(sequence1) != len(sequence2):
+            # If length is not the same return false
             return False
         else:
             return self.compare_words(sequence1.lower(), sequence2.lower())
 
     def compare_words(self, word1, word2):
+        """Compares two words to check if they are
+        made up of the same multiset of characters
+        using a dictionary of counters for every
+        letter in the alphabet.
+        """
         for character in word1:
             if character in self.table:
                 self.table[character] += 1
@@ -22,6 +39,7 @@ class AnagramChecker(object):
             if character in self.table:
                 self.table[character] -= 1
                 if self.table[character] < 0:
+                    # If counter is negative we can be sure words are not anagrams.
                     return False
             else:
                 raise Exception("Words to compare should only be made up of alphabetical characters.")
--- daniela997/assignment-1/anagramchecker_extended.py
+++ daniela997/assignment-1/anagramchecker_extended.py
@@ -1,8 +1,20 @@
+"""This module defines an Anagram checker implementation."""
 import string
 
 
 class AnagramChecker(object):
+    """Anagram checker with extended functionality.
+
+    Anagram sequences can be single words or whole sentences; white space
+    and punctuation is ignored.
+    """
     def __init__(self, is_case_sensitive=None, word_order_matters=None):
+        """Creates an anagram checker. It uses a hash table to perform checks.
+
+        Args:
+            is_case_sensitive: whether check should be case sensitive or not.
+            word_order_matters: whether order of letters is tied to words or not.
+        """
         if is_case_sensitive is None:
             self.is_case_sensitive = False
             self.table = dict.fromkeys(string.ascii_lowercase, 0)
@@ -19,11 +31,15 @@ class AnagramChecker(object):
             self.word_order_matters = word_order_matters
 
     def check_anagrams(self, sequence1, sequence2):
+        """Checks two sequences are anagrams.
+        """
         if not self.is_case_sensitive:
+            # If case does not matter convert to lowercase before check.
             sequence1 = sequence1.lower()
             sequence2 = sequence2.lower()
 
         if self.word_order_matters:
+            # If order matters split into list of strings to be matched.
             sequence2 = sequence2.split()
             sequence1 = sequence1.split()
 
@@ -31,6 +47,7 @@ class AnagramChecker(object):
                 return False
 
             for word1, word2 in zip(sequence1, sequence2):
+                # Perform check for each pair of words.
                 anagram_pair = self.compare_words(word1, word2)
 
                 self.table = self.table.fromkeys(self.table, 0)
@@ -42,6 +59,11 @@ class AnagramChecker(object):
             return self.compare_words(sequence1, sequence2)
 
     def compare_words(self, word1, word2):
+        """Compares two words to check if they are
+        made up of the same multiset of characters
+        using a dictionary of counters for every
+        letter in the alphabet.
+        """
         for character in word1:
             if character in self.table:
                 self.table[character] += 1

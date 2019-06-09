f8d238f07a305eca558303858325aa9940c20a0f
esparig <esparig@gmail.com>
2019-06-09 23:57:35 +0200

    get words from grid implemented.

--- esparig/assignment-3/word_search.py
+++ esparig/assignment-3/word_search.py
@@ -1,30 +1,59 @@
 """
 Assignment 3: Word Search.
 """
-from typing import List
+from typing import List, Set
 
 class Lexicon:
-    def __init__(self, chr: str, ends: bool, next: List['Lexicon']) -> None:
-        self.chr = chr
+    """Lexicon: collections of cords stored as a Trie.
+    """
+    def __init__(self, char: str, ends: bool, next_char: List['Lexicon']) -> None:
+        self.chr = char
         self.ends = ends
-        self.next = next
-    
-    def is_word(self, word: List[str]) -> bool:
-        """Takes a string as a list of characters and returns whether the string is a word in the lexicon
+        self.next = next_char
+
+    def is_word(self, word: str) -> bool:
+        """Takes a string and returns whether the string is a word in the lexicon.
         """
-        if len(word) == 0:
+        if not word:
             return self.ends
-        
+
         for elem in self.next:
             if word[0] == elem.chr:
                 return elem.is_word(word[1:])
-    
-    def is_preffix(self, preffix):
-        if len(preffix) == 0:
-            return True
-        
+
+    def is_prefix(self, prefix: str) -> bool:
+        """Takes a string and returns whether the string is a prefix of a word in the lexicon.
+        """
+        if not prefix:
+            if self.next:
+                return True
+            return False
+
         for elem in self.next:
-            if preffix[0] == elem.chr:
-                return elem.is_preffix(preffix[1:])
-        
-        
+            if prefix[0] == elem.chr:
+                return elem.is_prefix(prefix[1:])
+
+def words_from_grid(lexicon: Lexicon, grid: List[List[str]], rows: int, cols: int) -> Set[str]:
+    """Find the words from the lexicon that can be formed in the grid.
+    """
+    found_str = set()
+    def words_from_grid_helper(current_str: str, i: int, j: int, visited: List[List[bool]]) -> bool:
+        """Search and store words using DFS.
+        """
+        if lexicon.is_prefix(current_str):
+            for pos in [0, 1, 2, 3, 5, 6, 7, 8]:
+                pos_i, pos_j = pos//cols + i - 1, pos%cols + j - 1
+                if pos_i >= 0 and pos_j >= 0 and pos_i < rows and pos_j < cols:
+                    if not visited[pos_i][pos_j]:
+                        visited[pos_i][pos_j] = True
+                        words_from_grid_helper(current_str+grid[pos_i][pos_j],
+                                               pos_i, pos_j, visited)
+                        visited = [[False]*cols]*rows
+        if lexicon.is_word(current_str):
+            found_str.add(current_str)
+
+    for i in range(rows):
+        for j in range(cols):
+            current_str = grid[i][j]
+            words_from_grid_helper(current_str, i, j, [[False]*cols]*rows)
+    return found_str
--- esparig/assignment-3/word_search_test.py
+++ esparig/assignment-3/word_search_test.py
@@ -1,38 +1,41 @@
 """Unit tests for Assignment 3"""
 import unittest
 from typing import List
-from word_search import Lexicon
+from word_search import Lexicon, words_from_grid
+
 
 class TestWordSearch(unittest.TestCase):
     """Unit Test Class for Binary Tree.
     """
 
     def setUp(self):
-        
+
         def create_lexicon(words: List[str]) -> Lexicon:
+            """Initialize a lexicon.
+            """
             dummy_head = Lexicon(None, False, [])
             for word in words:
                 lexicon = dummy_head
-                for w in word:
-                    if len(lexicon.next) == 0:
-                        elem = Lexicon(w, False, [])
+                for char in word:
+                    if not lexicon.next:
+                        elem = Lexicon(char, False, [])
                         lexicon.next.append(elem)
                     else:
                         found = False
                         for elem in lexicon.next:
-                            if w == elem.chr:
+                            if char == elem.chr:
                                 found = True
                                 break
                         if not found:
-                            elem = Lexicon(w, False, [])          
+                            elem = Lexicon(char, False, [])
                             lexicon.next.append(elem)
                     lexicon = elem
                 lexicon.ends = True
             return dummy_head
-        
+
         self.my_lexicon = create_lexicon(["CAR", "CARD", "CART", "CAT"])
-        print(self.my_lexicon.chr, self.my_lexicon.next[0].chr, self.my_lexicon.next[0].next[0].chr) 
-        
+        self.my_grid = [['A', 'A', 'R'], ['T', 'C', 'D']]
+
     def test_is_word(self):
         """Test if a given word is in the lexicon
         """
@@ -40,17 +43,23 @@ class TestWordSearch(unittest.TestCase):
         self.assertTrue(self.my_lexicon.is_word("CARD"))
         self.assertTrue(self.my_lexicon.is_word("CART"))
         self.assertTrue(self.my_lexicon.is_word("CAT"))
-        
+
         self.assertFalse(self.my_lexicon.is_word("DOG"))
         self.assertFalse(self.my_lexicon.is_word("C"))
         self.assertFalse(self.my_lexicon.is_word("CA"))
         self.assertFalse(self.my_lexicon.is_word("CASH"))
-        
-    def test_is_preffix(self):
+
+    def test_is_prefix(self):
         """Test if a given word is in the lexicon
         """
-        self.assertTrue(self.my_lexicon.is_preffix("C"))
-        self.assertTrue(self.my_lexicon.is_preffix("CA"))
-        
-        self.assertFalse(self.my_lexicon.is_preffix("DOG"))
-        self.assertFalse(self.my_lexicon.is_preffix("CAS"))
+        self.assertTrue(self.my_lexicon.is_prefix("C"))
+        self.assertTrue(self.my_lexicon.is_prefix("CA"))
+
+        self.assertFalse(self.my_lexicon.is_prefix("DOG"))
+        self.assertFalse(self.my_lexicon.is_prefix("CAT"))
+
+    def test_words_from_grid(self):
+        """Test words from grid.
+        """
+        self.assertEqual(words_from_grid(self.my_lexicon, self.my_grid, 2, 3),
+                         set(["CAT", "CAR", "CARD"]))

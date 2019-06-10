8078479a3401b14082029c0cde2ab065db2f4cb9
Daniela Ivanova <daniela.ivanova21@gmail.com>
2019-06-10 23:25:48 +0100

    Added tests and comments.

--- daniela997/assignment-3/lexicon.py
+++ daniela997/assignment-3/lexicon.py
@@ -1,49 +1,95 @@
+"""Lexicon implementation uses the concept of a trie.
+"""
+
+
 class LetterNode:
     def __init__(self):
+        """Trie node, can have 0 or more children.
+        If the represented letter is also the last letter
+        in a word in the lexicon, the node is a terminating one.
+        """
         self.children = {}
         self.is_terminating = False
 
     def __repr__(self):
+        """Text representation for debugging purposes
+        - it does not print a nice-looking trie... :-)
+        """
         for child in self.children:
             print(self.children.get(child))
-        return ' | '.join(chr(child + ord('A')) for child in self.children)
+        return ' '.join(chr(child + ord('A')) for child in self.children)
 
 
 class Lexicon:
+    """A Lexicon is a trie built of LetterNodes.
+    """
     def __init__(self):
+        """Root is a parentless node.
+        """
         self.root = LetterNode()
 
     def __repr__(self):
         return repr(self.root)
 
     def get_child_index(self, child_character):
+        """Calculates an index under which a child node
+        will be stored in the self.children dictionary
+        for the parent.
+
+        :param child_character: child character to calculate
+        index for
+        :return: int representing the child's index
+        """
         return ord(child_character) - ord('A')
 
     def add_words(self, *args):
+        """
+        Adds to the Lexicon, representing each character as a
+        LetterNode.
+        :param args: words to be added
+        """
         for word in args:
             current_node = self.root
             for char in word:
+                # Traverse words character by character
                 index = self.get_child_index(char)
                 if index not in current_node.children:
+                    # If the character's index is not in the current node's children, add it
                     current_node.children[index] = LetterNode()
+                # Travel down the trie branch
                 current_node = current_node.children.get(index)
+            # When last character is reached, note that its node is a terminating one
             current_node.is_terminating = True
 
     def is_word(self, word):
+        """Checks if a word is in the Lexicon.
+        :param word: word to search for
+        :return: True/False if word is in the Lexicon or not
+        """
+        # Word is a prefix whose last character is a terminating node
         last_node = self._is_prefix_check(word)
         return True if last_node and last_node.is_terminating else False
 
-    def is_prefix(self, word):
-        return True if self._is_prefix_check(word) else False
+    def is_prefix(self, prefix):
+        """Checks if a string is a prefix to at least 1 word in the lexicon.
+        :param prefix: prefix to search for
+        :return: True/False if prefix is in the Lexicon or not
+        """
+        return True if self._is_prefix_check(prefix) else False
 
-    def _is_prefix_check(self, word):
-        if len(word) == 0:
-            return False
+    def _is_prefix_check(self, prefix):
+        """Checks if prefix is in lexicon. Helper used by is_prefix and is_word.
+        :param prefix: prefix to search for
+        :return: Last node of found prefix - None if prefix is not found
+        """
+        if len(prefix) == 0:
+            return None
         current_node = self.root
-        for char in word:
+        # For each character, walk down trie and check if prefix is represented as a trie branch
+        for char in prefix:
             index = self.get_child_index(char)
             if not current_node:
-                return False
+                return None
             current_node = current_node.children.get(index)
         return current_node
 
--- daniela997/assignment-3/main.py
+++ daniela997/assignment-3/main.py
@@ -1,3 +1,5 @@
+"""Example usage of lexicon.py and search_grid.py"""
+
 import sys
 
 from lexicon import Lexicon
@@ -7,8 +9,12 @@ from search_grid import search_grid
 def main(args):
     lexicon = Lexicon()
     lexicon.add_words("CAR", "CAT", "CARD", "CART")
+    lexicon2 = Lexicon()
+    lexicon2.add_words("CAT")
+    print(lexicon2)
     grid = [["A", "A", "R"], ["T", "C", "D"]]
     words = list(search_grid(grid, lexicon))
+    # Reversing so it prints results out in the order shown in the pdf
     words.reverse()
     print(words)
 

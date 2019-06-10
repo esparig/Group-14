0bb5d53f0970357ed112eaff4a34943a61008596
Daniela Ivanova <daniela.ivanova21@gmail.com>
2019-06-10 23:31:31 +0100

    Added suggested changes.

--- daniela997/assignment-1/anagramcheck_functions.py
+++ daniela997/assignment-1/anagramcheck_functions.py
@@ -22,13 +22,8 @@ def check_anagrams(sequence1, sequence2, is_case_sensitive=False, word_order_mat
         if len(sequence1) != len(sequence2):
             return False
 
-        for word1, word2 in zip(sequence1, sequence2):
-            # Perform check for each pair of words.
-            anagram_pair = compare_words(cleanup(word1), cleanup(word2))
-
-            if not anagram_pair:
-                return False
-        return True
+        return all(compare_words(cleanup(word1), cleanup(word2))
+                   for word1, word2 in zip(sequence1, sequence2))
     else:
         return compare_words(cleanup(sequence1), cleanup(sequence2))
 
@@ -46,4 +41,4 @@ def cleanup(sequence):
     """Cleans up character sequences, leaving only
     letters and numbers using regex.
     """
-    return re.sub('[^A-Za-z0-9]+', '', sequence)
+    return re.sub('\S', '', sequence)

696e71d2f7ca48e7d5bd22df68d77f452fcf9768
Veleslavia <okalistratova@gmail.com>
2019-06-13 11:21:39 +0200

    Container-like solution for assignment 2

--- veleslavia/assignment-2/binarytree.py
+++ veleslavia/assignment-2/binarytree.py
@@ -1,87 +1,39 @@
 
 
-class Node(object):
-    def __init__(self, key):
+class Node:
+    def __init__(self, key, left=None, right=None):
         self.key = key
-        self.left = None
-        self.right = None
-
-
-class BinaryTree(object):
-
-    def __init__(self):
-        self.root = None
+        self.left = Node(*(left[0], left[1:len(left)//2+1], left[len(left)//2+1:])) if left else None
+        self.right = Node(*(right[0], right[1:len(right)//2+1], right[len(right)//2+1:])) if right else None
 
     def __repr__(self):
-        self.traverse(self.root)
+        return ' '.join(str(key) for key in self)
 
-    def traverse(self, node):
-        if node is not None:
-            self.traverse(node.left)
-            print(node.key)
-            self.traverse(node.right)
-
-    def insert(self, key):
-        if self.root is None:
-            self.root = Node(key)
-        else:
-            self._insert_recursive(self.root, key)
-
-    def search(self, key):
-        return self._search_recursive(self.root, key)
+    def __iter__(self):
+        yield self.key
+        if self.left:
+            yield from self.left
+        if self.right:
+            yield from self.right
 
     def get_ancestors(self, key):
-        return self._get_ancestors_recursive(self.root, key)
+        return [ancestor for ancestor in self._ancestors(key)]
+
+    def _ancestors(self, key):
+        if key in self and self.key != key:
+            if self.left and self.left.key != key:
+                yield from self.left.get_ancestors(key)
+            if self.right and self.right.key != key:
+                yield from self.right.get_ancestors(key)
+            yield self.key
 
     def get_lowest_common_ancestor(self, key1, key2):
-        """ Returns common ancestor of key1 and key2
-        """
-        return self._get_lowest_common_ancestor(self.root, key1, key2)
 
-    def _insert_recursive(self, node, key):
-        if node is None:
-            node = Node(key)
-            return node
-        if key < node.key:
-            node.left = self._insert_recursive(node.left, key)
-        elif key > node.key:
-            node.right = self._insert_recursive(node.right, key)
-        return node
-
-    def _search_recursive(self, node, key):
-        if node is None or node.key == key:
-            return node
-        if node.key < key:
-            return self._search_recursive(node.right, key)
-        return self._search_recursive(node.left, key)
-
-    def _get_ancestors_recursive(self, node, key):
-        # TODO clean branches
-        if (node.left is None) and (node.right is None):
-            return list()
-        if node.left is not None:
-            if key == node.left.key:
-                return list([node.key])
-            left_ancestors = self._get_ancestors_recursive(node.left, key)
-            if left_ancestors:
-                left_ancestors.append(node.key)
-                return left_ancestors
-        if node.right is not None:
-            if key == node.right.key:
-                return list([node.key])
-            right_ancestors = self._get_ancestors_recursive(node.right, key)
-            if right_ancestors:
-                right_ancestors.append(node.key)
-                return right_ancestors
-
-    def _get_lowest_common_ancestor(self, node, key1, key2):
-        if node is None:
-            return node
-        if node.key == key1 or node.key == key2:
-            return node
-        left_lowest_common = self._get_lowest_common_ancestor(node.left, key1, key2)
-        right_lowest_common = self._get_lowest_common_ancestor(node.right, key1, key2)
-        if left_lowest_common and right_lowest_common:
-            return node
-
-        return left_lowest_common if left_lowest_common is not None else right_lowest_common
+        if key1 in self and key2 in self:
+            if self.key == key1 or self.key == key2:
+                return self
+            if (key1 in self.left and key2 in self.right) or (key2 in self.left and key1 in self.right):
+                return self
+            left_lowest_common = self.left.get_lowest_common_ancestor(key1, key2) if self.left else None
+            right_lowest_common = self.right.get_lowest_common_ancestor(key1, key2) if self.right else None
+            return left_lowest_common if left_lowest_common is not None else right_lowest_common
--- veleslavia/assignment-2/binarytree_test.py
+++ veleslavia/assignment-2/binarytree_test.py
@@ -1,33 +1,17 @@
 import unittest
 
-from binarytree import BinaryTree
+from binarytree import Node
 
 
 class BinaryTreeChecker(unittest.TestCase):
 
     def test_get_ancestors(self):
-        tree = BinaryTree()
-        tree.insert(5)
-        tree.insert(15)
-        tree.insert(10)
-        tree.insert(8)
-        tree.insert(2)
-        tree.insert(3)
-        tree.insert(18)
-        tree.insert(6)
+        tree = Node(*(5, (15, 10, 8), (2, 3, 18, 6)))
         self.assertEqual(tree.get_ancestors(2), [5])
-        self.assertEqual(tree.get_ancestors(8), [10, 15, 5])
+        self.assertEqual(tree.get_ancestors(18), [3, 2, 5])
 
     def test_get_lowest_common(self):
-        tree = BinaryTree()
-        tree.insert(5)
-        tree.insert(15)
-        tree.insert(10)
-        tree.insert(8)
-        tree.insert(2)
-        tree.insert(3)
-        tree.insert(18)
-        tree.insert(6)
+        tree = Node(*(5, (15, 10, 8), (2, 3, 18, 6)))
         self.assertEqual(tree.get_lowest_common_ancestor(2, 3).key, 2)
-        self.assertEqual(tree.get_lowest_common_ancestor(8, 18).key, 15)
+        self.assertEqual(tree.get_lowest_common_ancestor(8, 18).key, 5)
         self.assertEqual(tree.get_lowest_common_ancestor(3, 15).key, 5)

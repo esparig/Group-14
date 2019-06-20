8a00d651f962e00b66e400c88ecfd8b728e4adf8
esparig <kinahchan@gmail.com>
2019-06-20 17:34:17 +0200

    - added enum to territory types - adding graph based map to solve optional problem

--- esparig/assignment-4/counting_islands.py
+++ esparig/assignment-4/counting_islands.py
@@ -3,10 +3,53 @@ Assignment 4: Counting Islands.
 Given a 2-dimensional map of square tiles, compute the number of islands in the map.
 """
 from typing import List
+from enum import Enum
+
+class TypeTerritory:
+    """Enum to define types of tiles. Useful to avoid typing errors.
+    """
+    SEA = 0
+    LAND = 1
+
+class Territory:
+    """Node-like Data Structure for a territory. They will form the graph representation of a map.
+    """
+    def __init__(self, id: int, type: 'TypeTerritory', adjacents: List['Territory']):
+        self.id = id
+        self.type = type
+        self.adjacents = adjacents
+
+def count_islands_from_2d_map(map: List[List['TypeTerritory']]) -> int:
+    """Given a two-dimensional map, indicating Land or Sea terrain for each cell, 
+    this function returns the number of found islands and a representation identifying them.
+    """
+    width, height = len(map), len(map[0])
+    visited = [[None]*width for _  in range(height)]
+    
+    def explore_island(i: int, j: int, id_island: int):
+        """Explore the adjacent Land cells using DFS
+        """
+        if i >= 0 and i < width and j >= 0 and j < height:
+            if not visited[i][j] and map[i][j] == TypeTerritory.LAND:
+                visited[i][j] = id_island
+                for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                    explore_island(i+x, j+y, id_island)
+    
+    num_islands = 0
+    for i in range(width):
+        for j in range(height):
+            if not visited[i][j]:
+                if map[i][j] == TypeTerritory.LAND:
+                    num_islands += 1
+                    explore_island(i, j, num_islands)
+                else:
+                    visited[i][j] = 0
+                
+    return num_islands, visited
 
 def count_islands(map: List[List[str]]) -> int:
     """Given a two-dimensional map, indicating Land or Sea terrain for each cell, 
-    this function returns the number of found islands and a representation identifying them
+    this function returns the number of found islands and a representation identifying them.
     """
     width, height = len(map), len(map[0])
     visited = [[None]*width for _  in range(height)]
@@ -31,3 +74,6 @@ def count_islands(map: List[List[str]]) -> int:
                     visited[i][j] = 0
                 
     return num_islands, visited
+
+def count_islands_from_graph_map(map):
+    pass
--- esparig/assignment-4/counting_islands_test.py
+++ esparig/assignment-4/counting_islands_test.py
@@ -1,17 +1,43 @@
 """Unit tests for Assignment 4: Counting Islands"""
 import unittest
-from counting_islands import count_islands
+from counting_islands import TypeTerritory, count_islands, count_islands_from_2d_map
 
 MAP1 = [["Sea", "Land", "Sea", "Land"], 
        ["Land", "Land", "Sea", "Sea"],
        ["Sea", "Sea", "Land", "Sea"],
        ["Sea", "Sea", "Land", "Sea"]]
 
+MAP1b = [[TypeTerritory.SEA, TypeTerritory.LAND, TypeTerritory.SEA, TypeTerritory.LAND],
+         [TypeTerritory.LAND, TypeTerritory.LAND, TypeTerritory.SEA, TypeTerritory.SEA],
+         [TypeTerritory.SEA, TypeTerritory.SEA, TypeTerritory.LAND, TypeTerritory.SEA],
+         [TypeTerritory.SEA, TypeTerritory.SEA, TypeTerritory.LAND, TypeTerritory.SEA]]
+
 MAP2 = [["Sea", "Sea"],
         ["Sea", "Sea"]]
+MAP2b = [[TypeTerritory.SEA, TypeTerritory.SEA],
+         [TypeTerritory.SEA, TypeTerritory.SEA]]
 
 MAP3 = [["Land", "Land"],
         ["Land", "Land"]]
+MAP3b = [[TypeTerritory.LAND, TypeTerritory.LAND],
+         [TypeTerritory.LAND, TypeTerritory.LAND]]
+
+MAP4 = [[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],# 0
+        [1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],# 1
+        [1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],# 2
+        [1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],# 3
+        [0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0],# 4
+        [0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0],# 5
+        [0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0],# 6
+        [0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0],# 7
+        [0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1],# 8
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],# 9
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],#10
+        [0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0],#11
+        [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0],#12
+        [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1],#13
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1],#14
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1]]#15
 
 class TestCountingIslands(unittest.TestCase):
     """Assuming the map is well provided, we test the output of the count_islands function.
@@ -26,5 +52,18 @@ class TestCountingIslands(unittest.TestCase):
         
     def test_only_land_map(self):
         num_islands, new_map = count_islands(MAP3)
+        self.assertEqual(num_islands, 1, "Maybe we are in a continent!")
+    
+    def test_example_map(self):
+        num_islands, new_map = count_islands_from_2d_map(MAP1b)
+        self.assertEqual(num_islands, 3, "There are three islands!")
+        print(new_map)
+        
+    def test_only_sea_map(self):
+        num_islands, new_map = count_islands_from_2d_map(MAP2b)
+        self.assertEqual(num_islands, 0, "There isn't any island!")
+        
+    def test_only_land_map(self):
+        num_islands, new_map = count_islands_from_2d_map(MAP3b)
         self.assertEqual(num_islands, 1, "Maybe we are in a continent!")
         

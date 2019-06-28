b2283579f568bf1408da0e7e5f66cf9a39bee57c
Veleslavia <okalistratova@gmail.com>
2019-06-28 12:27:36 +0200

    Simplify the code, remove misleading sea map mutation, add "visited" structure instead, add docstring

--- veleslavia/assignment-4/arbitrary_seamap.py
+++
@@ -1,3 +0,0 @@
-class ArbitrarySeaMap:
-    """This class should take whatever data, create a graph
-    and search for a number of connected components in the graph """
--- veleslavia/assignment-4/seamap.py
+++ veleslavia/assignment-4/seamap.py
@@ -1,25 +1,27 @@
+from itertools import product
 from typing import List
 import unittest
 
 
-def process_island(sea_map: List[List[bool]], i: int, j: int):
+def process_island(sea_map: List[List[bool]], visited: List[List[bool]], i: int, j: int):
     connected_directions = [(0, -1), (0, 1), (1, 0), (-1, 0)]
-    sea_map[i][j] = False
+    visited[i][j] = True
     for (move_i, move_j) in connected_directions:
         if 0 <= i+move_i < len(sea_map) and 0 <= j+move_j < len(sea_map[i+move_i]):
-            if sea_map[i+move_i][j+move_j]:
-                process_island(sea_map, i+move_i, j+move_j)
+            if sea_map[i+move_i][j+move_j] and not visited[i+move_i][j+move_j]:
+                process_island(sea_map, visited, i+move_i, j+move_j)
+    return 1
 
 
 def count_islands(sea_map: List[List[bool]]) -> int:
-    n_islands = 0
-    for i in range(len(sea_map)):
-        for j in range(len(sea_map[i])):
-            # if land
-            if sea_map[i][j]:
-                n_islands += 1
-                process_island(sea_map, i, j)
-    return n_islands
+    """
+    Count number of islands in the map.
+    :param sea_map: 2D map of booleans where True represents LAND and False represents SEA
+    :return: number of islands in the map
+    """
+    visited = [[False] * len(row) for row in sea_map]
+    return sum(process_island(sea_map, visited, i, j) for (i, j) in
+               product(range(len(sea_map)), range(len(sea_map[0]))) if sea_map[i][j] and not visited[i][j])
 
 
 class CountIslandsTest(unittest.TestCase):

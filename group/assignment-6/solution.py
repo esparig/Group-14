9a23259b65bab5ebc4019d078c871327461df493
Veleslavia <okalistratova@gmail.com>
2019-07-23 18:30:52 +0200

    Get all possible solutions - Optional 2

--- group/assignment-6/solution.py
+++ group/assignment-6/solution.py
@@ -38,6 +38,20 @@ class ParkingState(dict):
                         history.append(new_state)
                         return new_state.rearrange_basic(end_state, history)
 
+    def rearrange_get_all(self, end_state, history: List):
+        if self == end_state:
+            yield history.copy()
+        else:
+            for parking_spot, car_id in self.items():
+                if self.inverse[car_id] != end_state.inverse[car_id]:
+                    new_state = ParkingState(self.copy())
+                    new_state[parking_spot], new_state[self.inverse[0]] = 0, car_id
+                    if new_state not in history:
+                        # print(f"Move car {car_id} from {self.inverse[car_id]} to {self.inverse[0]}")
+                        history.append(new_state)
+                        yield from new_state.rearrange_get_all(end_state, history)
+                        history.pop(-1)
+
 
 class TestParkingRearrangement(unittest.TestCase):
 
@@ -48,6 +62,17 @@ class TestParkingRearrangement(unittest.TestCase):
         movement_history = start_state.rearrange_basic(end_state, list())
 
         self.assertEqual(movement_history[-1], end_state)
+
+    def test_get_all_solutions(self):
+        start_state = ParkingState({'A': 1, 'B': 2, 'C': 0, 'D': 3})
+        end_state = ParkingState({'A': 3, 'B': 1, 'C': 2, 'D': 0})
+
+        movement_history = [i for i in start_state.rearrange_get_all(end_state, list())]
+        num_shortest_solution = sum(1 for i in movement_history if len(i) == 3)
+        num_longest_solution = sum(1 for i in movement_history if len(i) == 16)
+        self.assertEqual(num_shortest_solution, 1)
+        self.assertEqual(num_longest_solution, 2)
+        self.assertEqual(len(movement_history), 180)
 
     def test_restrictions(self):
         restrictions = {'A': [1, 2], 'D': [2, 3]}

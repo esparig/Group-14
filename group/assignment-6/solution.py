4ddba8c011a4af896b1bb77f7a3f80d9fa3d3645
Veleslavia <okalistratova@gmail.com>
2019-07-23 17:28:31 +0200

    Refactor rearrange with a new ParkingState bi-directional dict implementation

--- group/assignment-6/solution.py
+++ group/assignment-6/solution.py
@@ -6,37 +6,47 @@ import unittest
 
 
 class ParkingState(dict):
+
+    def __init__(self, *args, **kwargs):
+        super(ParkingState, self).__init__(*args, **kwargs)
+        self.restrictions = kwargs.pop('restrictions', dict())
+        self.inverse = {}
+        for key, value in self.items():
+            self.inverse.__setitem__(value, key)
+
+    def __setitem__(self, key, value):
+        super(ParkingState, self).__setitem__(key, value)
+        self.inverse.__setitem__(value, key)
+
+    def __delitem__(self, key):
+        self.inverse.__delitem__(self[key])
+        super(ParkingState, self).__delitem__(key)
+
     def __repr__(self):
         return f"{type(self).__name__}({super().__repr__()})"
 
-    def change_state(self, end_state: ParkingState, history: List[ParkingState]) -> List[ParkingState]:
+    def rearrange_basic(self, end_state, history):
         if self == end_state:
             return history
         else:
             for parking_spot, car_id in self.items():
-                if self[parking_spot]:
-                    if end_state[parking_spot] != car_id:
-                        new_state = ParkingState(self.copy())
-                        new_state[parking_spot] = None
-                        empty_keys = {key for key, value in self.items() if not value}
-                        move_to = empty_keys.pop()
-                        new_state[move_to] = car_id
-                        if new_state not in history:
-                            print(f"Move {car_id} from {parking_spot} to {move_to}")
-                            history.append(new_state)
-                            return new_state.change_state(end_state, history)
+                if self.inverse[car_id] != end_state.inverse[car_id]:
+                    new_state = ParkingState(self.copy())
+                    new_state[parking_spot] = 0
+                    new_state[self.inverse[0]] = car_id
+                    if new_state not in history:
+                        print(f"Move car {car_id} from {self.inverse[car_id]} to {self.inverse[0]}")
+                        history.append(new_state)
+                        return new_state.rearrange_basic(end_state, history)
 
 
 class TestParkingRearrangement(unittest.TestCase):
 
     def test_simple_solution(self):
-        car_ids = {1, 2, 3}
-        spot_ids = {'A', 'B', 'C', 'D'}
+        start_state = ParkingState({'A': 1, 'B': 2, 'C': 0, 'D': 3})
+        end_state = ParkingState({'A': 3, 'B': 1, 'C': 2, 'D': 0})
 
-        start_state = ParkingState({'A': 1, 'B': 2, 'C': None, 'D': 3})
-        end_state = ParkingState({'A': 3, 'B': 1, 'C': 2, 'D': None})
-
-        movement_history = start_state.change_state(end_state, list())
+        movement_history = start_state.rearrange_basic(end_state, list())
 
         self.assertEqual(movement_history[-1], end_state)
 

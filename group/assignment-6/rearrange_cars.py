a8d7a86f858cacb4290282d7a2fdb73b1a538a52
daniela997 <32989037+daniela997@users.noreply.github.com>
2019-07-28 22:48:10 +0100

    Some error handling, edge cases

--- group/assignment-6/rearrange_cars.py
+++ group/assignment-6/rearrange_cars.py
@@ -9,32 +9,21 @@ Assumptions:
 from typing import List
 import copy
 import sys
-from itertools import islice
-
-
-# Sliding window function used to print moves
-def window(seq, window_size=2):
-    """Returns a sliding window over data iterable"""
-    it = iter(seq)
-    result = tuple(islice(it, window_size))
-    if len(result) == window_size:
-        yield result
-    for elem in it:
-        result = result[1:] + (elem,)
-        yield result
-
+from random import randrange
+from util import window
 
 class ParkingState(dict):
     """
     Class to define the state of the parking using a dictionary.
-    Represented by a mappting from car to parking_lot.
+    Represented by a mapping from car to parking_lot.
+    Assumptions: input state can be mutated
     """
     def __init__(self, mapping: dict, reserved_for: dict = {}):
         super(ParkingState, self).__init__(mapping)
-        self.reserved_for = reserved_for
         self.inverse = {}
         for key, value in self.items():
             self.inverse.__setitem__(value, key)
+        self.restrictions = self.__get_restrictions__(reserved_for)
 
     def __setitem__(self, key, value):
         super(ParkingState, self).__setitem__(key, value)
@@ -46,7 +35,12 @@ class ParkingState(dict):
 
     def __repr__(self):
         return "{0}({1})".format(type(self).__name__, super().__repr__())
-    
+
+    def __get_restrictions__(self, reserved_for):
+        # Restrictions are the cars not allowed in a certain space i.e.
+        # all other cars that are not reserved for this space
+        return {lot_id: (set(range(1, len(self))) - set(reserved_for[lot_id])) if lot_id in reserved_for else set()
+                for car_id, lot_id in self.items()}
 
     def move_car(self, car_id: int):
         """
@@ -75,13 +69,20 @@ class ParkingState(dict):
         :param end_state: goal parking lot configuration
         :param history: a list of past parking lot configurations / states
         :return: Returns a sequence of configurations representing a path from start_state to end_state.
-
         """
         history = history + [copy.deepcopy(self)]
         while self != end_state:
+            if self[0] == end_state[0]:
+                # If there is a deadlock because of restrictions and the goal empty lot is currently empty,
+                # move a random car to the empty spot
+                if self[0] == end_state[0]:
+                    car_id = randrange(1, len(self))
+                    self.move_car(car_id)
+                    # append current state to history list
+                    history.append(copy.deepcopy(self))
             # For every car in lot, check that it's not yet in its correct lot and that it can be moved to the empty lot
             for car_id in range(1, len(self)):
-                if self[car_id] != end_state[car_id] and car_id in self.reserved_for[self[0]]:
+                if self[car_id] != end_state[car_id] and car_id not in self.restrictions[self[0]]:
                     self.move_car(car_id)
                     # append current state to history list
                     history.append(copy.deepcopy(self))
@@ -106,7 +107,8 @@ class ParkingState(dict):
             # if the lot that should be empty already is empty, swap it with a random car that needs to be moved
             if self[0] == end_state[0]:
                 car_to_move = [car_id for car_id in self if
-                               car_id in end_state and self[car_id] != end_state[car_id] and car_id in self.reserved_for[self[0]]].pop()
+                               car_id in end_state and self[car_id] != end_state[car_id]
+                               and car_id not in self.restrictions[self[0]]].pop()
                 self.move_car(car_to_move)
                 # append current state to history list
                 history.append(copy.deepcopy(self))
@@ -139,7 +141,7 @@ class ParkingState(dict):
         # i.e. parking lot configuration after swap has not been visited yet.
         # If yes, copy history so far and branch off
         for car_id in range(1, len(self)):
-            if self[car_id] != end_state[car_id] and car_id in self.reserved_for[self[0]]:
+            if self[car_id] != end_state[car_id] and car_id not in self.restrictions[self[0]]:
                 swap_state = copy.deepcopy(self)
                 swap_state.move_car(car_id)
                 if swap_state not in history:
@@ -155,7 +157,7 @@ class ParkingState(dict):
         """
         violates = False
         for car, parking_lot in self.items():
-            if car not in self.reserved_for[parking_lot]:
+            if car in self.restrictions[parking_lot]:
                 violates = True
                 break
         return violates
@@ -174,50 +176,71 @@ class ParkingState(dict):
             for car_id in range(1, len(self)):
                 self.move_car(car_id)
                 history.append(copy.deepcopy(self))
+            # if it still violates constrains, move a random car to get out of loop
+            if self.violates_constraints():
+                car_id = randrange(1, len(self))
+                self.move_car(car_id)
+                # append current state to history list
+                history.append(copy.deepcopy(self))
         return history
 
-    def print_moves(self, sequences: List['ParkingState']):
+    def __print_moves__(self, sequences: List['ParkingState']):
         """
         For a given list of sequences, print moves.
         :param sequences: list of sequences from start_state to end_state
         """
         for seq_pair in sequences:
             car_moved = seq_pair[0].find_car_given_lot(seq_pair[1][0])
-            print("Move car {0} from lot {1} to empty space at lot {2}".format(car_moved, seq_pair[1][0],
+            yield ("Move car {0} from lot {1} to empty space at lot {2}".format(car_moved, seq_pair[1][0],
                                                                                seq_pair[0][0]))
 
     # Printing helpers.
     def print_any_path(self, end_state: 'ParkingState'):
         path = self.rearrange_cars_any_path(end_state)
-        print("Path:")
-        self.print_moves(list(window(path)))
+        if len(path) > 1:
+            print("A move sequence to arrange cars in given state: ")
+            [print(move) for move in self.__print_moves__(list(window(path)))]
+        elif self.violates_constraints():
+            raise Exception("There are no move sequences that satisfy there requirements.")
+        elif self == end_state:
+            raise Exception("Start and end state are the same.")
 
     def print_shortest_path(self, end_state: 'ParkingState'):
         path = self.rearrange_cars_shortest_path(end_state)
-        print("Shortest path:")
-        self.print_moves(list(window(path)))
+        if len(path) > 1:
+            print("Shortest move sequence to arrange cars in given state:: ")
+            [print(move) for move in self.__print_moves__(list(window(path)))]
+        elif self.violates_constraints():
+            raise Exception("There are no move sequences that satisfy there requirements.")
+        elif self == end_state:
+            raise Exception("Start and end state are the same.")
 
     def print_all_paths(self, end_state: 'ParkingState'):
         paths = self.rearrange_cars_all_paths_distinct_states(end_state)
-        for i in range(0, len(paths)):
-            print("Path ", i+1)
-            sequences = [seq for seq in window(paths[i])]
-            self.print_moves(sequences)
+        if len(paths) > 1:
+            for i in range(0, len(paths)):
+                print("Move sequence to arrange cars in given state #{0}:".format(i+1))
+                sequences = [seq for seq in window(paths[i])]
+                [print(move) for move in self.__print_moves__(sequences)]
+        elif self.violates_constraints():
+            raise Exception("There are no move sequences that satisfy there requirements.")
+        elif self == end_state:
+            raise Exception("Start and end state are the same.")
 
     def print_path_to_satisfying_end_state(self):
         path = self.path_to_satisfying_end_state()
         if len(path) > 1:
-            print("Path:")
-            self.print_moves(list(window(path)))
-        else:
-            print("Start state satisfies constraints, so it is also an end state.")
-        print("End state: ", path[-1])
-
+            print("Move sequence to arrange cars in state {0}, which satisfies constraints: ".format(path[-1]))
+            [print(move) for move in self.__print_moves__(list(window(path)))]
+        elif self.violates_constraints():
+            raise Exception("There are no move sequences that satisfy there requirements.")
+        elif self == path[-1]:
+            raise Exception("Start and end state are the same.")
 
 def main(args):
-    start_state = ParkingState({0: 'B', 1: 'A', 2: 'C', 3: 'D'}, reserved_for={'A': [0, 1, 2, 3], 'B': [0, 1, 2, 3], 'C': [0, 1], 'D': [0, 2, 3]})
-    end_state = ParkingState({0: 'C', 1: 'B', 2: 'D', 3: 'A'})
-    start_state.print_path_to_satisfying_end_state()
+    start_state = ParkingState({0: 'C', 1: 'A', 2: 'B', 3: 'D'})
+    end_state = ParkingState({0: 'B', 1: 'C', 2: 'A', 3: 'D'})
+    start_state.print_any_path(end_state)
 
 
 if __name__ == '__main__':
